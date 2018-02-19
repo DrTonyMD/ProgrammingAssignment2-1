69e8ad329a9741c40afaf6fd77dfd664414c34e7
DrTonyMD <36278658+DrTonyMD@users.noreply.github.com>
2018-02-19 15:17:23 -0500

    This is the assignment for week number three of the Corsera R-programming course

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+##  Programming assignment - week 3
+## 
 
-## Write a short comment describing this function
+## This creates a special matrix object
 
 makeCacheMatrix <- function(x = matrix()) {
-
+     m<-NULL
+     set <- function(y) {
+          x<<-y
+          m<<-NULL
+     }
+     get <- function() x
+     setinverse <- function(inverse) m <<- inverse
+     getinverse <- function() m
+     list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
 }
 
-
-## Write a short comment describing this function
+## Computes the inverse of the special matrix.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+     m<- x$getinverse()
+     if(!is.null(m)){
+          message("getting cached inverse matrix")
+          return(m)
+     }
+     data<- x$get()
+     m<- solve(data)
+     x$setinverse(m)
+     m
 }
+
+## The problem comes in when one tries to test the functions. 
+## If you simply call makeCacheMatrix without assigning it to a matrix object, 
+## and then try to call cacheSolve on the same matrix, an error is generated:
+##      
+##      "Error in x$getinverse : $ operator is invalid for atomic vectors"
+## 
+## This drove me crazy, and should not have been part of the assignment. 
+## The trick is to assign the return of  makeCacheMatrix to another matrix, 
+## and then execute cacheSolve on that matrix (leaving your original matrix intact, 
+## by the way:). 
+## 
+## I could still use more explanation surrounding this code, especially 
+## for generating the special matrix object.
+## 
